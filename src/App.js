224906593c7a3e4f5609a883fcf9d1a986db97f5
unknown <yu23691188@gmail.com>
2021-10-21 15:12:14 -0400

    change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <img
         width={80}
         height={80}
-        src={ "https://res.cloudinary.com/dz42u9age/image/upload/chansgourmetnj/163461930076016346193007" }
+        src={ "http://res.cloudinary.com/dz42u9age/image/upload/chansgourmetnj/163461930076016346193007" }
       />
     </div>
   );
